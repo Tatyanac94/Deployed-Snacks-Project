ab05032052b1b22ff2cde4ee0428133d3ff27135
Tatyanac94 <ms.cuttino94@gmail.com>
2024-08-31 18:29:24 -0400

    updated root path

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+app.get('/welcome', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'welcome.html'));
 });
 
