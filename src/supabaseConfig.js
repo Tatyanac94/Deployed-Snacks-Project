76f0c57b7108324bcca6ef2a89761875eafbc643
Tatyanac94 <ms.cuttino94@gmail.com>
2024-09-03 00:43:04 -0400

    update timeout

--- src/supabaseConfig.js
+++ src/supabaseConfig.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const axiosInstance = axios.create({
   baseURL: process.env.SUPABASE_URL + '/rest/v1',
-  timeout: 5000,
+  timeout: 1000,
   headers: {
     apikey: process.env.SUPABASE_KEY,
     Authorization: `Bearer ${process.env.SUPABASE_KEY}`,
