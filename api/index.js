424ec107fcb83198b4140361872b37457ccb1a10
Tatyanac94 <ms.cuttino94@gmail.com>
2024-09-15 18:57:07 -0400

    fixed file paths

--- api/index.js
+++ api/index.js
@@ -1,3 +1,22 @@
+// const express = require('express');
+// const router = express.Router();
+
+// // Import the snacks router
+// const snacksRouter = require('./snacks');
+
+// // Use the snacks router for routes starting with /snacks
+// router.use('/snacks', snacksRouter);
+
+// module.exports = router;
+
+
+
+
+
+
+
+
+
 const express = require('express');
 const router = express.Router();
 
