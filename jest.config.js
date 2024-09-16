6248972c879c860f42f1b302b251ee833605a5a7
Tatyanac94 <ms.cuttino94@gmail.com>
2024-09-15 23:22:28 -0400

    updated jest

--- jest.config.js
+++ jest.config.js
@@ -1,34 +1,4 @@
-// module.exports = {
-//     rootDir: './',
-  
-//     coverageDirectory: 'coverage',
-  
-//     testMatch: [
-//       '**/__tests__/**/*.js',
-//       '**/?(*.)+(spec|test).js'
-//     ],
-  
-//     transform: {
-//       '^.+\\.js$': 'babel-jest'
-//     },
-  
-//     moduleFileExtensions: ['js', 'json'],
-  
-//     clearMocks: true,
-  
-//     verbose: true,
-  
-//     transformIgnorePatterns: ['<rootDir>/node_modules/'],
-  
-//     setupFiles: ['dotenv/config'],
-//   };
-  
-
-
-
-// jest.config.js
 module.exports = {
     testEnvironment: 'node',
     verbose: true,
-    // Other Jest configuration options if needed
 };
